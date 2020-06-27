35eab70e22d83d3dbd04171fd953f24bea22316b
ollefrost <elcasaboxi@gmail.com>
2020-06-27 13:45:20 +0800

    added 'home' screen (php + css)

---
+++ home.php
@@ -0,0 +1,18 @@
+<html>
+    <head>
+        <meta charset="UTF-8">
+        <title>Glory Florist</title>
+        <link rel="stylesheet" href="home.css">
+    </head>
+    <body>
+
+        <div id='container'>
+          <a href="#"><img id='feature' src='https://i.postimg.cc/d1fJr9k2/sakura.png'></a>
+          <div id='links'>
+            <a class='link' href='#'>shop</a>
+            <a class='link' href='#'>account</a>
+          </div>
+        </div>
+
+    </body>
+</html>
