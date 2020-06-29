a570df6e697490c7866a1c9b443c1d46318ed045
Johann <mast3r1sgr3at@hotmail.com>
2020-06-29 12:48:33 +0800

    StaffOrders (not done)

---
+++ Views/staffOrders.php
@@ -0,0 +1,58 @@
+<html>
+    <head>
+        <meta charset="UTF-8">
+        <title>Glory Florist : View Orders</title>
+        <link rel="stylesheet" href="common.css">
+        <link rel="stylesheet" href="staffOrders.css">
+    </head>
+    <body>
+        <div id='container'> <!-- <form id='container'> ??? -->
+
+            <div id='hotbar'>
+                <a href='#' id='glory'>glory florist</a>
+                <a href='#' class='link'>shop</a>
+                <a href='#' class='link'>cart</a>
+                <a href='#' class='link' >account</a>
+                <a href='#' class='link' id='currentLink'>dashboard</a>
+            </div>
+
+            <div id='top'>
+                <div id='text'>
+                    <a href='staffDashboard.php' id='back'>back to dashboard</a>
+                    <a id='title'>View Orders</a>
+                </div>
+            </div>
+            <div id="content">
+                <div class="masonryContainer">
+                    <div class='item'>
+                        Item 1 <br> asdkajsldkasjl
+                    </div>
+                    <div class='item' id="high">
+                        Item<br><br><br>ss<br><br>ss
+                    </div>
+                    <div class='item'>
+                        Item 1
+                    </div>
+                    <div class='item'>
+                        Item 1
+                    </div>
+                    <div class='item'>
+                        Item 1
+                    </div>
+                    <div class='item'>
+                        Item<br><br><br>ss<br><br>ss
+                    </div>
+                    <div class='item'>
+                        Item 1
+                    </div>
+                    <div class='item'>
+                        Item 1
+                    </div>
+
+
+                </div>
+            </div>
+        </div>
+    </body>
+</html>
+
