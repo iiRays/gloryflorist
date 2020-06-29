c4ad085c308f316d66eab8108dc953016100a984
Johann <mast3r1sgr3at@hotmail.com>
2020-06-29 09:43:00 +0800

    Fixed a bug

---
+++ Views/staffDashboard.php
@@ -0,0 +1,28 @@
+<html>
+    <head>
+        <meta charset="UTF-8">
+        <title>Glory Florist : Staff Dashboard</title>
+        <link rel="stylesheet" href="common.css">
+    </head>
+    <body>
+        <div id='container'> <!-- <form id='container'> ??? -->
+
+            <div id='hotbar'>
+                <a href='#' id='glory'>glory florist</a>
+                <a href='#' class='link'>shop</a>
+                <a href='#' class='link' id='currentLink'>cart</a>
+                <a href='#' class='link'>account</a>
+            </div>
+
+            <div id='top'>
+                <div id='text'>
+                    <a href='#' id='back'>back to the&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;shop</a>
+                    <a id='title'>Your Cart</a>
+                </div>
+            </div>
+            <div id="content">
+            </div>
+        </div>
+    </body>
+</html>
+
