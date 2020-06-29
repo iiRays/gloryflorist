d675ad3bc49872866d4bac6f3d61edc19978fd7c
Johann <mast3r1sgr3at@hotmail.com>
2020-06-29 12:30:54 +0800

    Added staffDashboard

--- Views/staffDashboard.php
+++ Views/staffDashboard.php
@@ -3,6 +3,7 @@
         <meta charset="UTF-8">
         <title>Glory Florist : Staff Dashboard</title>
         <link rel="stylesheet" href="common.css">
+        <link rel="stylesheet" href="staffDashboard.css">
     </head>
     <body>
         <div id='container'> <!-- <form id='container'> ??? -->
@@ -10,17 +11,29 @@
             <div id='hotbar'>
                 <a href='#' id='glory'>glory florist</a>
                 <a href='#' class='link'>shop</a>
-                <a href='#' class='link' id='currentLink'>cart</a>
-                <a href='#' class='link'>account</a>
+                <a href='#' class='link'>cart</a>
+                <a href='#' class='link' >account</a>
+                <a href='#' class='link' id='currentLink'>dashboard</a>
             </div>
 
             <div id='top'>
                 <div id='text'>
-                    <a href='#' id='back'>back to the&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;shop</a>
-                    <a id='title'>Your Cart</a>
+                    <a href='#' id='back'>back &nbsp; to &nbsp; the shop</a>
+                    <a id='title'>Staff Dashboard</a>
                 </div>
             </div>
             <div id="content">
+                <div id="left">
+                    <div class="item">
+                        View orders
+                        <div id="counter">7</div>
+                    </div>
+                </div>
+                <div id="right">
+                    <div class="item">
+                        Manage flowers
+                    </div>
+                </div>
             </div>
         </div>
     </body>
