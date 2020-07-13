24f2a7ab2d6ceed45a7a63ab897d67a9ee910642
iiRays <40648726+iiRays@users.noreply.github.com>
2020-07-13 13:49:53 +0800

    Update staffOrders.php

--- Views/staffOrders.php
+++ Views/staffOrders.php
@@ -27,25 +27,27 @@
 
                 <div class="masonryContainer">
                     <?php
-                    $order1 = '<div class="item" id="order1" data-arrangementcount="4">
-                        <div class="orderID" >O2938</div> 
-                        <div class="arrangement" id="order1arrangement1" data-quantity="3" data-name="Extreme bouquet" data-stalk="12" data-flower="Rose">Extreme bouquet x 3</div> 
-                        <div class="flowers">12 stalks</div>
-                        <div class="flowers">Rose</div>
-                        <div class="arrangement" id="order1arrangement2" data-quantity="2" data-name="Supreme Flowers" data-stalk="10" data-flower="Lily">Supreme Flowers x 2</div> 
-                        <div class="flowers">10 stalks</div>
-                        <div class="flowers">Lily</div>
-                        <div class="arrangement" id="order1arrangement3" data-quantity="3" data-name="Extreme bouquet" data-stalk="12" data-flower="Rose">Extreme bouquet x 3</div> 
-                        <div class="flowers">12 stalks</div>
-                        <div class="flowers">Rose</div>
-                        <div class="arrangement" id="order1arrangement4" data-quantity="2" data-name="Supreme Flowers" data-stalk="10" data-flower="Lily">Supreme Flowers x 2</div> 
-                        <div class="flowers">10 stalks</div>
-                        <div class="flowers">Lily</div>
-                        <div class="deadline">3 hours left</div>
+                    //Generate an order
+                    $id = "order2";
+                    $order1 = "<div class=\"item\" id=\"$id\" data-arrangementcount=\"4\">
+                        <div class=\"orderID\" >$id</div> 
+                        <div class=\"arrangement\" id=\"{$id}arrangement1\" data-quantity=\"3\" data-name=\"Extreme bouquet\" data-stalk=\"12\" data-flower=\"Rose\">Extreme bouquet x 3</div> 
+                        <div class=\"flowers\">12 stalks</div>
+                        <div class=\"flowers\">Rose</div>
+                        <div class=\"arrangement\" id=\"{$id}arrangement2\" data-quantity=\"2\" data-name=\"Supreme Flowers\" data-stalk=\"10\" data-flower=\"Lily\">Supreme Flowers x 2</div> 
+                        <div class=\"flowers\">10 stalks</div>
+                        <div class=\"flowers\">Lily</div>
+                        <div class=\"arrangement\" id=\"{$id}arrangement3\" data-quantity=\"3\" data-name=\"Extreme bouquet\" data-stalk=\"12\" data-flower=\"Rose\">Extreme bouquet x 3</div> 
+                        <div class=\"flowers\">12 stalks</div>
+                        <div class=\"flowers\">Rose</div>
+                        <div class=\"arrangement\" id=\"{$id}arrangement4\" data-quantity=\"2\" data-name=\"Supreme Flowers\" data-stalk=\"10\" data-flower=\"Lily\">Supreme Flowers x 2</div> 
+                        <div class=\"flowers\">10 stalks</div>
+                        <div class=\"flowers\">Lily</div>
+                        <div class=\"deadline\">3 hours left</div>
                     </div>
-                </div>';
-                            
-                     echo $order1;
+                </div>";
+
+                    echo $order1;
                     ?>
                 </div>
             </div>
@@ -57,40 +59,57 @@
 
                 </div>
 
-                <div class="deadline">3 hours left</div>
+                <div class="bottomBar">
+                    <div class="deadline">3 hours left</div>
+                    <div class="orderStatus">
+                        <div class="pending selected">Pending</div>
+                        <div class="doing">Doing</div>
+                        <div class="delivering">Delivering</div>
+                        <div class="dropped">Dropped</div>
+                    </div>
+                </div>
             </div>
+                </body>
+                <script>
+                    $(".item").click(function () {
+                        $(".itemFocus").addClass("itemFocusVisible");
+                        $(".overlay").addClass("overlayVisible");
 
-    </body>
-    <script>
-        $(".item").click(function () {
-            $(".itemFocus").addClass("itemFocusVisible");
-            $(".overlay").addClass("overlayVisible");
+                        //get order ID
+                        var id = this.id;
+                        $("#itemFocusOrderID").html(id);
+                        var arrangementCount = $("#" + id).data("arrangementcount");
+                        var arrangementContainer = "";
 
-            //get order ID
-            var id = this.id;
-            $("#itemFocusOrderID").html(id);
-            var arrangementCount = $("#" + id).data("arrangementcount");
-            var arrangementContainer = "";
+                        for (var i = 0; i < arrangementCount; i++) {
+                            var quantity = $("#" + id + "arrangement" + (i + 1)).data("quantity");
+                            var name = $("#" + id + "arrangement" + (i + 1)).data("name");
+                            var flower = $("#" + id + "arrangement" + (i + 1)).data("flower");
+                            var stalk = $("#" + id + "arrangement" + (i + 1)).data("stalk");
 
-            for (var i = 0; i < arrangementCount; i++) {
-                var quantity = $("#" + id + "arrangement" + (i + 1)).data("quantity");
-                var name = $("#" + id + "arrangement" + (i + 1)).data("name");
-                var flower = $("#" + id + "arrangement" + (i + 1)).data("flower");
-                var stalk = $("#" + id + "arrangement" + (i + 1)).data("stalk");
+                            arrangementContainer += "<div class='arrangement'>" + name + "<div class='flowers'>" + stalk + " stalks</div> <div class='flowers'>" + flower + "</div><div class='arrangementQuantity'>x" + quantity +"</div></div>";
+                        }
 
-                arrangementContainer += "<div class='arrangement'>" + name + "<div class='arrangementQuantity'>" + quantity + "</div></div><div class='flowers'>" + stalk + " stalks</div> <div class='flowers'>" + flower + "</div>";
-            }
+                        // Set values into itemFocus
+                        $(".itemFocus > .arrangementContainer").html(arrangementContainer);
+                    });
+                    $(".overlay").click(function () {
+                        $(".itemFocus").removeClass("itemFocusVisible");
+                        $(".overlay").removeClass("overlayVisible");
+                    });
+                    $(".orderStatus div").click(function () {
+                        $(".orderStatus div").removeClass("selected");
+                        $(this).addClass("selected");
+                    });
+                    $(".orderStatus div").mouseover(function () {
+                        $(this).addClass("hover");
+                    });
+                    $(".orderStatus div").mouseleave(function () {
+                        $(this).removeClass("hover");
+                    });
+                    $(document).ready(function () {
 
-            // Set values into itemFocus
-            $(".itemFocus > .arrangementContainer").html(arrangementContainer);
-        });
-        $(".overlay").click(function () {
-            $(".itemFocus").removeClass("itemFocusVisible");
-            $(".overlay").removeClass("overlayVisible");
-        });
-        $(document).ready(function () {
-
-        });
-    </script>
-</html>
+                    });
+                </script>
+                </html>
 
