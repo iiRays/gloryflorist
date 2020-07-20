9d76098f9731fb34d61161d6ac47ee965ef26a72
Johann <mast3r1sgr3at@hotmail.com>
2020-07-20 18:00:28 +0800

    Added RedBeansPHP library

--- test.php
+++ test.php
@@ -1,22 +1,9 @@
 <?php
-include "Model/FloralArrangement.php";
 include "Controllers/Util/DB.php";
+include "Controllers/Util/rb.php";
 
-//$flower = new Flower();
-//$flower->flowerID = "TEST";
-//$flower->flowerType = "Stalk";
-//$flower->flowerName = "Rose";
-//$flower->description = "TESTDesc";
-//$flower->price = 123;
-//
-//$flower->save();
 
-//I want get floral arrangement
-$conn = DB::connectDB();
-$sql = $conn->prepare("select * from floralArrangement where floralArrangementID = :id");
-$id = "Test";
-$sql->bindParam(":id", $id);
-$result = DB::get($sql, $conn, "FloralArrangement");
 
-//I want to know what flowers are used
-echo ($result[0]->flowerItemCollection[0]->flower->flowerName);
+R::setup('mysql:host=localhost;dbname=gloryfloristdb', 'root', ''); //for both mysql or mariaDB
+$flower = R::find("flora", "flower_id = ?", ['TEST']);
+echo $flower[1]->flowerName;
