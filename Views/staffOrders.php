51fcaab7861a0753f4ea1f7b457c0315de34b05d
Johann <mast3r1sgr3at@hotmail.com>
2020-07-24 11:38:31 +0800

    View staff orders functionality

--- Views/staffOrders.php
+++ Views/staffOrders.php
@@ -27,27 +27,55 @@
 
                 <div class="masonryContainer">
                     <?php
-                    //Generate an order
-                    $id = "order2";
-                    $order1 = "<div class=\"item\" id=\"$id\" data-arrangementcount=\"4\">
-                        <div class=\"orderID\" >$id</div> 
-                        <div class=\"arrangement\" id=\"{$id}arrangement1\" data-quantity=\"3\" data-name=\"Extreme bouquet\" data-stalk=\"12\" data-flower=\"Rose\">Extreme bouquet x 3</div> 
-                        <div class=\"flowers\">12 stalks</div>
-                        <div class=\"flowers\">Rose</div>
-                        <div class=\"arrangement\" id=\"{$id}arrangement2\" data-quantity=\"2\" data-name=\"Supreme Flowers\" data-stalk=\"10\" data-flower=\"Lily\">Supreme Flowers x 2</div> 
-                        <div class=\"flowers\">10 stalks</div>
-                        <div class=\"flowers\">Lily</div>
-                        <div class=\"arrangement\" id=\"{$id}arrangement3\" data-quantity=\"3\" data-name=\"Extreme bouquet\" data-stalk=\"12\" data-flower=\"Rose\">Extreme bouquet x 3</div> 
-                        <div class=\"flowers\">12 stalks</div>
-                        <div class=\"flowers\">Rose</div>
-                        <div class=\"arrangement\" id=\"{$id}arrangement4\" data-quantity=\"2\" data-name=\"Supreme Flowers\" data-stalk=\"10\" data-flower=\"Lily\">Supreme Flowers x 2</div> 
-                        <div class=\"flowers\">10 stalks</div>
-                        <div class=\"flowers\">Lily</div>
-                        <div class=\"deadline\">3 hours left</div>
-                    </div>
-                </div>";
+                    include "../Controllers/Util/rb.php";
 
-                    echo $order1;
+                    R::setup('mysql:host=localhost;dbname=flowerdb', 'root', ''); //for both mysql or mariaDB
+                    $orderList = R::findAll("orders");
+                    $order = "";
+
+                    foreach ($orderList as $individualOrder) {
+                        
+                        $id = $individualOrder->id;
+                       
+                        //Get order item
+                        $orderItemList = R::find("orderitem", "order_id = ?", [$id]);
+                         $count = count($orderItemList);
+                        $order .= "<div class=\"item\" id=\"{$id}\" data-arrangementcount=\"{$count}\">
+                        <div class=\"orderID\" >order {$id}</div>";
+
+                        $counter = 0;
+                        foreach ($orderItemList as $orderItem) {
+                            $arrangement = $orderItem->arrangement;
+                            $counter += 1;
+                            $order .= "  <div class=\"arrangement\" id=\"{$id}arrangement{$counter}\" data-quantity=\"{$orderItem->quantity}\" data-name=\"{$arrangement->name}\" data-stalk=\"{$arrangement->stalks}\" data-flower=\"{$arrangement->flower->flowerName}\">{$arrangement->name} x {$orderItem->quantity}</div> 
+                        <div class=\"flowers\">{$arrangement->stalks} stalks</div>
+                        <div class=\"flowers\">{$arrangement->flower->flowerName}</div>";
+                        }
+                        
+                        $order .= "<div class=\"deadline\">3 hours left</div></div></div>";
+                   }
+//
+//                    //Generate an order
+//                    $id = "order2";
+//                    $order1 = "<div class=\"item\" id=\"$id\" data-arrangementcount=\"4\">
+//                        <div class=\"orderID\" >$id</div> 
+//                        <div class=\"arrangement\" id=\"{$id}arrangement1\" data-quantity=\"3\" data-name=\"Extreme bouquet\" data-stalk=\"12\" data-flower=\"Rose\">Extreme bouquet x 3</div> 
+//                        <div class=\"flowers\">12 stalks</div>
+//                        <div class=\"flowers\">Rose</div>
+//                        <div class=\"arrangement\" id=\"{$id}arrangement2\" data-quantity=\"2\" data-name=\"Supreme Flowers\" data-stalk=\"10\" data-flower=\"Lily\">Supreme Flowers x 2</div> 
+//                        <div class=\"flowers\">10 stalks</div>
+//                        <div class=\"flowers\">Lily</div>
+//                        <div class=\"arrangement\" id=\"{$id}arrangement3\" data-quantity=\"3\" data-name=\"Extreme bouquet\" data-stalk=\"12\" data-flower=\"Rose\">Extreme bouquet x 3</div> 
+//                        <div class=\"flowers\">12 stalks</div>
+//                        <div class=\"flowers\">Rose</div>
+//                        <div class=\"arrangement\" id=\"{$id}arrangement4\" data-quantity=\"2\" data-name=\"Supreme Flowers\" data-stalk=\"10\" data-flower=\"Lily\">Supreme Flowers x 2</div> 
+//                        <div class=\"flowers\">10 stalks</div>
+//                        <div class=\"flowers\">Lily</div>
+//                        <div class=\"deadline\">3 hours left</div>
+//                    </div>
+//                </div>";
+
+                    echo $order;
                     ?>
                 </div>
             </div>
@@ -69,47 +97,47 @@
                     </div>
                 </div>
             </div>
-                </body>
-                <script>
-                    $(".item").click(function () {
-                        $(".itemFocus").addClass("itemFocusVisible");
-                        $(".overlay").addClass("overlayVisible");
+    </body>
+    <script>
+        $(".item").click(function () {
+            $(".itemFocus").addClass("itemFocusVisible");
+            $(".overlay").addClass("overlayVisible");
 
-                        //get order ID
-                        var id = this.id;
-                        $("#itemFocusOrderID").html(id);
-                        var arrangementCount = $("#" + id).data("arrangementcount");
-                        var arrangementContainer = "";
+            //get order ID
+            var id = this.id;
+            $("#itemFocusOrderID").html("order " + id);
+            var arrangementCount = $("#" + id).data("arrangementcount");
+            var arrangementContainer = "";
 
-                        for (var i = 0; i < arrangementCount; i++) {
-                            var quantity = $("#" + id + "arrangement" + (i + 1)).data("quantity");
-                            var name = $("#" + id + "arrangement" + (i + 1)).data("name");
-                            var flower = $("#" + id + "arrangement" + (i + 1)).data("flower");
-                            var stalk = $("#" + id + "arrangement" + (i + 1)).data("stalk");
+            for (var i = 0; i < arrangementCount; i++) {
+                var quantity = $("#" + id + "arrangement" + (i + 1)).data("quantity");
+                var name = $("#" + id + "arrangement" + (i + 1)).data("name");
+                var flower = $("#" + id + "arrangement" + (i + 1)).data("flower");
+                var stalk = $("#" + id + "arrangement" + (i + 1)).data("stalk");
 
-                            arrangementContainer += "<div class='arrangement'>" + name + "<div class='flowers'>" + stalk + " stalks</div> <div class='flowers'>" + flower + "</div><div class='arrangementQuantity'>x" + quantity +"</div></div>";
-                        }
+                arrangementContainer += "<div class='arrangement'>" + name + "<div class='flowers'>" + stalk + " stalks</div> <div class='flowers'>" + flower + "</div><div class='arrangementQuantity'>x" + quantity + "</div></div>";
+            }
 
-                        // Set values into itemFocus
-                        $(".itemFocus > .arrangementContainer").html(arrangementContainer);
-                    });
-                    $(".overlay").click(function () {
-                        $(".itemFocus").removeClass("itemFocusVisible");
-                        $(".overlay").removeClass("overlayVisible");
-                    });
-                    $(".orderStatus div").click(function () {
-                        $(".orderStatus div").removeClass("selected");
-                        $(this).addClass("selected");
-                    });
-                    $(".orderStatus div").mouseover(function () {
-                        $(this).addClass("hover");
-                    });
-                    $(".orderStatus div").mouseleave(function () {
-                        $(this).removeClass("hover");
-                    });
-                    $(document).ready(function () {
+            // Set values into itemFocus
+            $(".itemFocus > .arrangementContainer").html(arrangementContainer);
+        });
+        $(".overlay").click(function () {
+            $(".itemFocus").removeClass("itemFocusVisible");
+            $(".overlay").removeClass("overlayVisible");
+        });
+        $(".orderStatus div").click(function () {
+            $(".orderStatus div").removeClass("selected");
+            $(this).addClass("selected");
+        });
+        $(".orderStatus div").mouseover(function () {
+            $(this).addClass("hover");
+        });
+        $(".orderStatus div").mouseleave(function () {
+            $(this).removeClass("hover");
+        });
+        $(document).ready(function () {
 
-                    });
-                </script>
-                </html>
+        });
+    </script>
+</html>
 
---
+++ Views/test.php
@@ -0,0 +1,66 @@
+<?php
+include "../Controllers/Util/rb.php";
+
+R::setup('mysql:host=localhost;dbname=flowerdb', 'root', ''); //for both mysql or mariaDB
+//$flower = R::find("flower", "flower_id = ?", ['TEST']);
+
+//$flower = R::load('flower', 'F01');
+//echo $flower;
+
+
+
+/*$num = R::count('flower');
+$flower = R::load('flower', 1);
+//$flowers = R::find('flower',' flowerID = ? ', ['F01']);
+echo $num . "<br>";
+echo var_dump($flower);
+echo $flower->flowerID;*/
+R::nuke();
+
+$flower = R::dispense("flower");
+$flower->flowerName = "Rose";
+$flower->price = 12.00;
+$flower->description = "Very beautiful";
+
+$flower2 = R::dispense("flower");
+$flower2->flowerName = "Lily";
+$flower2->price = 12.00;
+$flower2->description = "Very beautiful";
+
+$arrangement = R::dispense("arrangement");
+$arrangement->name = "Rose Bouquet";
+$arrangement->cost = "100";
+$arrangement->wrapColor = "red";
+$arrangement->flower = $flower;
+$arrangement->stalks = 12;
+
+$arrangement2 = R::dispense("arrangement");
+$arrangement2->name = "Gay Flowers";
+$arrangement2->cost = "130";
+$arrangement2->wrapColor = "blue";
+$arrangement2->flower = $flower2;
+$arrangement2->stalks = 9;
+//
+//$item = R::dispense("item");
+//$item->flower = $flower;
+//$item->arrangement = $arrangement;
+//$item->quantity = 12;
+
+$order = R::dispense("orders");
+$order->grandTotal = 100.00;
+$order->deliveryAddress = "Sabah";
+$order->status = "pending";
+
+
+// Each arrangement, 1 order item
+$orderItem = R::dispense("orderitem");
+$orderItem->arrangement = $arrangement;
+$orderItem->order = $order;
+$orderItem->quantity = 2;
+
+$orderItem2 = R::dispense("orderitem");
+$orderItem2->arrangement = $arrangement2;
+$orderItem2->order = $order;
+$orderItem2->quantity = 4;
+
+R::storeAll([$flower, $flower2, $arrangement, $arrangement2, $order, $orderItem, $orderItem2]);
