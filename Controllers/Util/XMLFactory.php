a58e4cb3f40fe8f859e4562891dfec0a17722522
Johann <mast3r1sgr3at@hotmail.com>
2020-08-14 17:17:48 +0800

    Streamlined XMLFactory

--- Controllers/Util/XMLFactory.php
+++ Controllers/Util/XMLFactory.php
@@ -8,7 +8,31 @@ require_once("XML.php");
 
 class XMLFactory {
 
-    public function createXML($rootName) {
+    public function build($rootName) {
+        // Create the XML
+        $xml = $this->buildXML($rootName);
+
+        return $xml;
+    }
+
+    public function buildFromDatabase($tableName) {
+        // Create the XML
+        $xml = $this->buildXML($tableName."List"); // Root node will be eg. userList
+        
+        // If this is an XML meant from database
+        // Check if redbeanphp is already setup
+        if (!R::testConnection()) {
+            R::setup('mysql:host=localhost;dbname=flowerdb', 'root', '');
+        }
+        
+        // Root name MUST BE EQUAL to a database table name for this to work
+        $result = R::findAll($tableName);
+        $xml->appendXML($result, $tableName);
+        
+        return $xml;
+    }
+    
+    private function buildXML($rootName){
         $xml = new XML();
         $xml->createRoot($rootName);
         return $xml;
--- Views/viewStaff.php
+++ Views/viewStaff.php
@@ -3,7 +3,6 @@
 require_once("../Controllers/Util/rb.php");
 require_once("../Controllers/Util/XML.php");
 require_once("../Controllers/Util/XMLFactory.php");
-require_once("../Controllers/Util/XMLDatabaseFactory.php");
 
 R::setup('mysql:host=localhost;dbname=flowerdb', 'root', ''); //for both mysql or mariaDB
 
@@ -12,7 +11,7 @@ $changelog = R::findAll("changelog", "order by id desc limit 3");
 $changelog = array_reverse($changelog);
 
 $factory = new XMLFactory();
-$xml = $factory->createXML("viewStaff");
+$xml = $factory->build("viewStaff");
 $xml->appendXML($staffList, "staff");
 $xml->appendXML($changelog, "changelog");
 echo $xml->styleWith("staffMemberStyle.xsl");
