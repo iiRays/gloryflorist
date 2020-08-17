de66b1cd86c2413ba5ea0867d302557db7b5173a
yong haw quan <hawquan@gmail.com>
2020-08-18 01:05:29 +0800

    account profile

--- Views/AccountDetails.php
+++ Views/AccountDetails.php
@@ -1,10 +1,41 @@
 <?php
-
 require_once("../Controllers/Util/Quick.php");
 require_once("../Controllers/Util/rb.php");
 require_once("../Controllers/Security/Session.php");
 require_once("../Controllers/Util/DB.php");
 
+$errors = array();
 DB::connect();
 
-Session::get($key);
+$currentAccount = Session::get("user");
+$name = $currentAccount["name"];
+$email = $currentAccount["email"];
+$phone = $currentAccount["phone"];
+$address = $currentAccount["address"];
+
+
+if (isset($_POST['btnDone'])) {
+
+    $name = Quick::getPostData("name");
+    $email = Quick::getPostData("email");
+    $phone = Quick::getPostData("phone");
+    $address = Quick::getPostData("address");
+
+    //validation
+    if (empty($name)) {
+        array_push($errors, "Name is required");
+    }
+    if (empty($email)) {
+        array_push($errors, "Email is required");
+    }
+
+    if (count($errors) == 0) {
+        $user = Session::get("user");
+        $user->name = $name;
+        $user->phone = $phone;
+        $user->address = $address;
+        
+        R::store($user);
+        header('location: Account.php');
+    }
+}
