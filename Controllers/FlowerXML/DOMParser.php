927d2f5edec73e0ffb36f9910911c284276fc14b
jacksonwjc99 <weijie11@hotmail.com.tw>
2020-08-20 20:22:23 +0800

    xml update

--- Controllers/FlowerXML/DOMParser.php
+++ Controllers/FlowerXML/DOMParser.php
@@ -1,7 +1,8 @@
 <?php
+
 /*
- Author: Chong Wei Jie
- ID: 19WMR09574
+  Author: Chong Wei Jie
+  ID: 19WMR09574
  * 
  */
 
@@ -10,23 +11,25 @@ require_once 'Flower.php';
 class DOMParser {
 
     private $flowers;
+    private $xlstProc;
 
-    public function __construct($filename) {
+    public function __construct($xmlFile, $xslFile) {
+        $this->xlstProc = new XSLTProcessor();
         $this->flowers = new SplObjectStorage();
-        $this->readFromXML($filename);
-        $this->display();
+        $this->readFromXML($xmlFile);
+        $this->display($xmlFile, $xslFile);
     }
 
-    public function readFromXML($filename) {
-        $xml = simplexml_load_file($filename);
+    public function readFromXML($xmlFile) {
+        $xml = simplexml_load_file($xmlFile);
         $flowersList = $xml->flower;
 
         foreach ($flowersList as $flower) {
             $attr = $flower->attributes();
             $flowerTemp = new Flower($flower->id, $flower->flower_name, $flower->remark, $flower->img, $flower->is_available);
-            if($flower->is_available == "true"){
+            if ($flower->is_available == "true") {
                 $flower->is_available = "Yes";
-            }else{
+            } else {
                 $flower->is_available = "No";
             }
 
@@ -34,15 +37,21 @@ class DOMParser {
         }
     }
 
-    public function display() {
-        echo '<h2>Flower List </h2>' 
-        . '<table border="1" style="text-align:center;"><tr><th>ID</th><th>Image</th><th>Flower name</th><th>Remarks</th><th>Available for floral arrangement</th></tr>';
-        foreach ($this->flowers as $flower) {
-            print $flower . "<br />";
-        }
-        echo '</table>';
+    public function display($xmlFile, $xslFile) {
+        $proc = new XsltProcessor;
+        $proc->importStylesheet(DOMDocument::load($xslFile)); //load XSL script
+        echo $proc->transformToXML(DOMDocument::load($xmlFile));
+        /*
+          echo '<h2>Flower List </h2>'
+          . '<table border="1" style="text-align:center;"><tr><th>ID</th><th>Image</th><th>Flower name</th><th>Remarks</th><th>Available for floral arrangement</th></tr>';
+          foreach ($this->flowers as $flower) {
+          print $flower . "<br />";
+          }
+          echo '</table>';
+         * 
+         */
     }
 
 }
 
-$flower = new DOMParser("Flower.xml");
+$flower = new DOMParser("Flower.xml", "Flower.xsl");
