9f9b799e53057c8def0d37eb7b184765aebe31c9
yong haw quan <hawquan@gmail.com>
2020-08-22 01:36:05 +0800

    ...

--- Views/registerCustomer.php
+++ Views/registerCustomer.php
@@ -20,19 +20,19 @@ Authorize::onlyAllow("guest");
             <?php include('errors.php'); ?>
             <div class="input-group">
                 <label>Name</label>
-                <input type="text" name="name">
+                <input type="text" name="name" value="<?php echo isset($_POST['name']) ? $_POST['name'] : '' ?>" >
             </div>
             <div class="input-group">
                 <label>Email</label>
-                <input type="text" name="email"> 
+                <input type="text" name="email" value="<?php echo isset($_POST['email']) ? $_POST['email'] : '' ?>" > 
             </div>
             <div class="input-group">
                 <label>Password</label>
-                <input type="password" name="password_1">
+                <input type="password" name="password_1" value="<?php echo isset($_POST['password_1']) ? $_POST['password_1'] : '' ?>">
             </div>
             <div class="input-group">
                 <label>Confirm Password</label>
-                <input type="password" name="password_2">
+                <input type="password" name="password_2" value="<?php echo isset($_POST['password_2']) ? $_POST['password_2'] : '' ?>">
             </div>
             <div class="input-group">
                 <button type="submit" name="register" class="btn">Register</button>
