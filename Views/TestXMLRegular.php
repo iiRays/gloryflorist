3409d9da7e8a614e8fc1352d862e7d87e5acd847
Johann <mast3r1sgr3at@hotmail.com>
2020-08-22 20:53:44 +0800

    Some changes

--- Views/TestXMLRegular.php
+++ Views/TestXMLRegular.php
@@ -7,8 +7,13 @@ require_once("../Controllers/Util/DB.php");
 
 DB::connect();
 
-$result = R::find("arrangement", "is_available = ?", ["true"]);
+// Constructing the XML
 $factory = new XMLFactory();
 $xml = $factory->construct("regular", "arrangementList");
+
+// Loading data into XML
+$result = R::find("arrangement", "is_available = ?", ["true"]);
 $xml->appendXML($result, "arrangements");
+
+// Display XML
 $xml->display();
