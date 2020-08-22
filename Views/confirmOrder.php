cf766382b0e578783d40fd63e301ceffbfc2e869
ollefrost <elcasaboxi@gmail.com>
2020-08-22 17:23:55 +0800

    half-done xml stuff in confirmOrder.php

--- Views/confirmOrder.php
+++ Views/confirmOrder.php
@@ -27,8 +27,64 @@ foreach ($cart->items as $item) {
 $totalPriceArr = []; // lazy coder moment
 $totalPriceArr[] = new CartItem("Total price", 1, $totalPrice); // there's only ever gonna be 1 item in totalPriceArr
 
-$factory = new XMLFactory("confirmOrder");
+/*$factory = new XMLFactory("confirmOrder");
 $xml = $factory->build();
 $xml->appendXML($cartItems, "items");
 $xml->appendXML($totalPriceArr, "totalPriceArr");
-echo $xml->styleWith("confirmOrderStyle.xsl");
+echo $xml->styleWith("confirmOrderStyle.xsl");*/
+
+$xml = new DOMDocument();
+$xmlRoot = $xml->documentElement;
+$xml->appendChild($xml->createElement("confirmOrder"));
+
+/*$mainNode = $xml->createElement("items"."s");
+
+foreach ($cartItems as $item) {
+    // Create a node
+    $node = $xml->appendChild($xml->createElement("items"));
+
+    // Each data inside node
+    foreach ($item as $key => $value) {
+
+        // Create a String node
+        $dataNode = $xml->createElement($key);
+        $dataNode->appendChild($xml->createTextNode($value));
+
+        // Add each inner node into the outer node
+        $node->appendChild($dataNode);
+    }
+
+    // Put the node inside the entire result
+    $mainNode->appendChild($node);
+    $xmlRoot->appendChild($mainNode);
+}
+
+$mainNode = $xml->createElement("totalPriceArr"."s");
+
+foreach ($totalPriceArr as $item) {
+    // Create a node
+    $node = $xml->appendChild($xml->createElement("totalPriceArr"));
+
+    // Each data inside node
+    foreach ($item as $key => $value) {
+
+        // Create a String node
+        $dataNode = $xml->createElement($key);
+        $dataNode->appendChild($xml->createTextNode($value));
+
+        // Add each inner node into the outer node
+        $node->appendChild($dataNode);
+    }
+
+    // Put the node inside the entire result
+    $mainNode->appendChild($node);
+    $xmlRoot->appendChild($mainNode);
+}*/
+
+$xsl = new DOMDocument();
+$xsl->load("confirmOrderStyle.xsl");
+
+$xlstProc = new XSLTProcessor();
+$xlstProc->importStylesheet($xsl);
+
+echo $xlstProc->transformToXml($xml);
