3dd0a8577cf3fb7f3a540f2cc614ff29734b7971
kelvintyh <kelvinthammZZLeo@gmail.com>
2020-08-23 14:17:12 +0800

    delivery address optional fix

--- Views/deliveryAddress.php
+++ Views/deliveryAddress.php
@@ -50,7 +50,7 @@ Authorize::onlyAllow("customer"); //temperory disable for better coding envir
                             </label>
                         </div>
                         <div class="input">           
-                            <input type="text" name="apartment-suite-unit-etc" required="off" autocomplete="false" value="<?php echo isset($_POST['apartment-suite-unit-etc']) ? htmlspecialchars($_POST['apartment-suite-unit-etc'], ENT_QUOTES) : ''; ?>"> 
+                            <input type="text" name="apartment-suite-unit-etc" required="false" autocomplete="false" value="<?php echo isset($_POST['apartment-suite-unit-etc']) ? htmlspecialchars($_POST['apartment-suite-unit-etc'], ENT_QUOTES) : ''; ?>"> 
                             <label for="apartment-suite-unit-etc" class="label-name">
                                 <span class="content-name">Apartment, suite, unit etc. (optional)</span>
                             </label>
