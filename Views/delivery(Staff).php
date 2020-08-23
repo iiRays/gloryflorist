5ae7862c78237d7eec5a41f9df1113c456268db8
ollefrost <elcasaboxi@gmail.com>
2020-08-23 13:17:00 +0800

    design updates for delivery module

--- Views/delivery(Staff).php
+++ Views/delivery(Staff).php
@@ -13,34 +13,44 @@ require_once("../Controllers/Util/Quick.php");
     <head>
         <title>Glory Florist : delivery list</title>
         <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
-        <link  rel="stylesheet" href="CSS/">
+        <link  rel="stylesheet" href="CSS/deliveryStaff.css">
     </head>
     <body>
         <div id='container'>
-            <?php Quick::printHeader("staffDashboard") ?>
-            <ol>
-                <!--For self entry delivery or pickup -->
-                <li><a href="../Views/delivery.php">Create New Delivery or Pickup</a></li>
-                <li><a href="../Views/deliveryReport(Staff).php">View Delivery and Pickup summary report</a></li>
-                <li><a href="../Views/delivery(Staff).php" name="list">View Delivery list</a></li>
-            </ol>
-            <!-- Display today delivery and pickup -->
-            <div>
-                <form method="post" action="delivery(Staff).php">
-                    <label>Select a date and click "go" to view the delivery list</label>
-                    <input type="date" name="date" 
-                           value="">
-                    <button >Go</button>
-                </form>
+            <?php Quick::printHeader("delivery(Staff)") ?>
+            
+            <div id='top'>
+                <div id='text'>
+                    <a href='staffDashboard.php' id='back'>back to staff dashboard</a>
+                    <a id='title'>Delivery (Staff)</a>
+                </div>
             </div>
-            <?php
-            //call here..
-            $header = "<h1>Delivery List</h1>"
-                    . "<h3>For Date: " . Quick::getPostData("date") . "</h3>"
-                    . "<h4>Please deliver the flower to our beloved one, and Ensure them pickup their flower Ya. </h4>";
-            echo $header;
-            $deliveryList = new XSLTTransformation("deliveries.xml", "delivery_list_on_date.xsl");
-            ?>
+            
+            <div id='content'>
+                <ol>
+                    <!--For self entry delivery or pickup -->
+                    <li><a href="../Views/delivery.php">Create New Delivery or Pickup</a></li>
+                    <li><a href="../Views/deliveryReport(Staff).php">View Delivery and Pickup summary report</a></li>
+                    <li><a href="../Views/delivery(Staff).php" name="list">View Delivery list</a></li>
+                </ol>
+                <!-- Display today delivery and pickup -->
+                <div>
+                    <form method="post" action="delivery(Staff).php">
+                        <label>Select a date and click "go" to view the delivery list</label>
+                        <input type="date" name="date" 
+                               value="">
+                        <button >Go</button>
+                    </form>
+                </div>
+                <?php
+                //call here..
+                $header = "<h1>Delivery List</h1>"
+                        . "<h3>For Date: " . Quick::getPostData("date") . "</h3>"
+                        . "<h4>Please deliver the flower to our beloved one, and Ensure them pickup their flower Ya. </h4>";
+                echo $header;
+                $deliveryList = new XSLTTransformation("deliveries.xml", "delivery_list_on_date.xsl");
+                ?>
+            </div>
         </div>
     </body>
 </html>
--- Views/deliveryReport(Staff).php
+++ Views/deliveryReport(Staff).php
@@ -12,33 +12,46 @@ $style = "";
 <!DOCTYPE html>
 <html>
     <head>
-        <title>Glory Florist :</title>
+        <title>Glory Florist : delivery report</title>
         <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
-        <link  rel="stylesheet" href="CSS/">
+        <link  rel="stylesheet" href="CSS/deliveryReportStaff.css">
     </head>
     <body>
-        <ol>
-            <!--For self entry delivery or pickup -->
-            <li><a href="../Views/delivery.php">Create New Delivery or Pickup</a></li>
-            <li><a href="../testFiles/deliveryReport(Staff).php">View Delivery and Pickup summary report</a></li>
-            <li><a href="../testFiles/delivery(Staff).php" name="list">View Delivery list</a></li>
-        </ol>
-        <!-- Display today delivery and pickup -->
-        <div>
-            <form method="post" action="deliveryReport(Staff).php">
-                <label>Select date range and click "go" to view the Delivery Summary Report</label>
-                <input type="date" name="start" value="">
-                <input type="date" name="end" value="">
-                <button >Go</button>
-            </form>
-        </div>
-        <?php
-        $header = "<h1>Delivery Summary Report</h1>"
-                . "<h3>For Date   : " . Quick::getPostData("start") . " util " . Quick::getPostData("end") . "</h3>"
-                . "<h4>Prepared by: </h4>";
-        echo $header;
+        <div id='container'>
+            <?php Quick::printHeader("deliveryReport(Staff)") ?>
+            
+            <div id='top'>
+                <div id='text'>
+                    <a href='staffDashboard.php' id='back'>back to staff dashboard</a>
+                    <a id='title'>Delivery Report (Staff)</a>
+                </div>
+            </div>
+            
+            <div id='content'>
+                <ol>
+                    <!--For self entry delivery or pickup -->
+                    <li><a href="../Views/delivery.php">Create New Delivery or Pickup</a></li>
+                    <li><a href="../testFiles/deliveryReport(Staff).php">View Delivery and Pickup summary report</a></li>
+                    <li><a href="../testFiles/delivery(Staff).php" name="list">View Delivery list</a></li>
+                </ol>
+                <!-- Display today delivery and pickup -->
+                <div>
+                    <form method="post" action="deliveryReport(Staff).php">
+                        <label>Select date range and click "go" to view the Delivery Summary Report</label>
+                        <input type="date" name="start" value="">
+                        <input type="date" name="end" value="">
+                        <button >Go</button>
+                    </form>
+                </div>
+                <?php
+                $header = "<h1>Delivery Summary Report</h1>"
+                        . "<h3>For Date   : " . Quick::getPostData("start") . " util " . Quick::getPostData("end") . "</h3>"
+                        . "<h4>Prepared by: </h4>";
+                echo $header;
 
-        $sr = new XSLTTransformation("deliveries.xml", "delivery_summary_report.xsl");
-        ?>
+                $sr = new XSLTTransformation("deliveries.xml", "delivery_summary_report.xsl");
+                ?>
+            </div>
+        </div>
     </body>
 </html>
